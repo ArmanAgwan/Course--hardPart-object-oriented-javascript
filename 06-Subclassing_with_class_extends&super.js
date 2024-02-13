cbbe568ae64ba328b222920c9350569e466b0660
ArmanAgwan <armanagwa@gmail.com>
2024-02-13 14:09:13 +0530

    code

---
+++ 06-Subclassing_with_class_extends&super.js
@@ -0,0 +1,36 @@
+// Create Object With class //
+
+// class userCreator {
+//   constructor(name, score) {
+//     this.name = name;
+//     this.score = score;
+//   }
+//   sayName() {
+//     console.log("I am" + this.name);
+//   }
+//   increment() {
+//     this.score++;
+//   }
+// }
+
+// const user1 = new userCreator("Phil", 4);
+// const user2 = new userCreator("Tim", 5);
+
+// user1.sayName();
+
+// 2 //
+
+// class paidUserCreator extends userCreator {
+//   constructor(paidName, paidScore, accountBalance) {
+//     super(paidName, paidScore);
+//     this.accountBalance = accountBalance;
+//   }
+//   increaseBalance() {
+//     this.accountBalance++;
+//   }
+// }
+
+// const paidUser1 = new paidUserCreator("Alyssa", 8, 25);
+
+// paidUser1.increaseBalance();
+// paidUser1.sayName();
